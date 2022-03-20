39345e32b6758c2a0b32374291d44fee05b7bc2c
Jose Maria CL <chema9703@gmail.com>
2022-03-19 18:39:48 -0600

    Add hook to easily use the services layer queries

---
+++ src/hooks/use-service.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { usePrevious } from "./use-previous";
+
+/**
+ * @param {function} serviceFunction Fetch function
+ * @param {object} args Params passed to the function
+ * @param {{ shouldFetch: boolean }} suspense If passed, hook won't try
+ * to perform the fetch until shouldFetch property is true
+ */
+export function useService(
+  serviceFunction,
+  args = {},
+  suspense = undefined
+) {
+  const [state, setState] = useState({
+    loading: true,
+    data: null,
+    error: null
+  });
+
+  const setLoading = (loading = false) => {
+    setState(prevState => ({ ...prevState, loading }));
+  };
+
+  const setError = (error = null) => {
+    setState(prevState => ({ ...prevState, error }));
+  };
+
+  const setData = (data = null) => {
+    setState(prevState => ({ ...prevState, data }));
+  };
+
+  const performQuery = async newArgs => {
+    setLoading(true);
+    try {
+      const data = await serviceFunction(newArgs || args);
+      setData(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Needed to check only once the suspense config to avoid
+  // an infinite loop
+  const initialShouldFetch = usePrevious(suspense?.shouldFetch);
+
+  useEffect(() => {
+    if (suspense === undefined)
+      performQuery().then();
+    else if (suspense?.shouldFetch !== initialShouldFetch
+      && !!suspense?.shouldFetch) {
+      performQuery().then();
+    }
+  }, [suspense]);
+
+  return { ...state, refetch: performQuery };
+}
