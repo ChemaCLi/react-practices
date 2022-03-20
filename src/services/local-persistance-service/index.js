b004fe0ef837bcb632cdcffeb6ebcd6e31d3bb44
Jose Maria CL <chema9703@gmail.com>
2022-03-19 18:36:21 -0600

    Add service to save data in localStorage

---
+++ src/services/local-persistance-service/index.js
@@ -0,0 +1,48 @@
+const sleep = (ms) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+const saveData = async (entity, data) => {
+  const items = await getData(entity) ?? []
+
+  const updatedItems = updateItemInItemsById(data, items)
+  localStorage.setItem(entity, JSON.stringify(updatedItems))
+
+  return data;
+}
+
+const updateItemInItemsById = (item, items = []) => {
+  const idx = items.findIndex((i) => i.id === item.id)
+
+  if (idx === -1) {
+    items.push(item)
+    return items
+  }
+
+  const prevItem = items[idx];
+  const newItem = { ...prevItem, ...item }
+  items[idx] = newItem
+  return items
+}
+
+const getData = async (entity) => {
+  await sleep(500)
+  return JSON.parse(localStorage.getItem(entity))
+}
+
+const removeItemById = async (entity, id) => {
+  const items = await getData(entity) ?? []
+  const idx = items.findIndex((i) => i.id === id)
+
+  if (idx === -1) return items
+  items.splice(idx, 1)
+  localStorage.setItem(entity, JSON.stringify(items))
+
+  return items
+}
+
+export const LocalPersistanceService = {
+  saveData,
+  getData,
+  removeItemById
+}
+
