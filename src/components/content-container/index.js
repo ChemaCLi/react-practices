8c1b1b77db460a0839b9968cf4b7172b64ba308a
Jose Maria CL <chema9703@gmail.com>
2022-03-20 05:01:37 -0600

    Improve container

--- src/components/content-container/index.js
+++ src/components/content-container/index.js
@@ -1,11 +1,12 @@
 export const ContentContainer = ({ children }) => {
   return (
     <div style={{
-      width: "70%",
+      maxWidth: "700px",
       margin: "2rem auto",
       display: "flex",
       flexDirection: "column",
-      gap: 50
+      gap: 50,
+      padding: "0.5rem"
     }}>
       {children}
     </div>
